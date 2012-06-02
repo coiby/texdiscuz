bc30094ff722db36efe6fe69bb8a3dccb0c86bf8
coiby <coiby.xu@gmail.com>
2012-06-03 01:42:48 +0800

    supporing manually configure API of MathJax and limit rendering to specified CURMODULEs

--- MathJax_DX2/mathjax.class.php
+++ MathJax_DX2/mathjax.class.php
@@ -5,29 +5,30 @@ if(!defined('IN_DISCUZ')) {
 /**
  * Discuz plugin for MathJax
  * use $$ or \[\] to wrap texcodes
- * @copyright	© 2010 ZJUSTU
+ * @copyright	© 2012 ZJUSTU
  * @author		Coiby@zjustu [http://zjustu.org]
  * @since		version - 2012-6-2
+ * @License GPL2
  */
 class plugin_mathjax {
 	var $pluginurl = '';
+	var $mathjax_api = '';
 	public function __construct() {
 		global $_G;
-
+		if( ($this->mathjax_api=$_G['cache']['plugin']['mathjax']['mathjax_api'])==''){
+			$this->mathjax_api = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+		}
 		$this->pluginurl = $_G['siteurl'].'source/plugin/mathjax/';
 	}
 	function global_footer(){
-		$script = "<script type='text/x-mathjax-config'>\n
-  MathJax.Hub.Config({\n
-    tex2jax: {\n
-      inlineMath: [ ['$','$'], [\"\\\\[\",\"\\\\]\"] ],\n
-      processEscapes: true\n
-    }\n
-  });\n
-</script>";
-		$script .= "<script type='text/javascript' src='/MathJax/MathJax.js?config=TeX-AMS_HTML-full'></script>\n";
-		
+		if(CURMODULE == 'viewthread' || CURMODULE == 'forumdisplay' || CURMODULE == 'post') {
+		$script = "<script type='text/x-mathjax-config'>\n MathJax.Hub.Config({\n    tex2jax: {
+      inlineMath: [ ['$','$'], [\"\\\\[\",\"\\\\]\"], ['[tex]','[/tex]'] ],\n processEscapes: true\n}\n });\n </script>";
+		$script .= "<script type='text/javascript' src='".$this->mathjax_api."'></script>\n";
 		return $script;
+		} else{
+			return;
+		}
 	}
 }
 
